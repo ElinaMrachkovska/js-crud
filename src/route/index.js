2c9282a4bceb9828bcadebe3424f99e8310a8e6e
ElinaMrachkovska <elinamrachkovska@gmail.com>
2025-02-06 20:45:41 +0200

    06/02

--- src/route/index.js
+++ src/route/index.js
@@ -25,23 +25,24 @@ class Product {
   static getById = (id) =>
     this.#list.find((product) => product.id === id)
 
-  static updateById = (id, { name }) => {
-    const product = this.getById(id)
-    if (product) {
-      if (name) {
-        product.name = name
-      }
-      return true
-    } else {
-      return false
-    }
-  }
   static deleteById = (id) => {
     const index = this.#list.findIndex(
       (product) => product.id === id,
     )
     if (index !== -1) {
       this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static updateById = (id, { name }) => {
+    const product = this.getById(id)
+    if (product) {
+      if (name) {
+        product.name = name
+      }
       return true
     } else {
       return false
@@ -68,7 +69,7 @@ router.get('/', function (req, res) {
   // ↑↑ сюди вводимо JSON дані
 })
 
-router.post('/product-create', function (req, res) {
+router.get('/product-create', function (req, res) {
   const { name, price, description } = req.body
   // console.log(req.body)
   const product = new Product(name, price, description)
@@ -83,6 +84,20 @@ router.post('/product-create', function (req, res) {
   })
 })
 
+router.post('/product-create', function (req, res) {
+  const { name, price, description } = req.body
+  // console.log(req.body)
+  const product = new Product(name, price, description)
+  Product.add(product)
+
+  console.log(Product.getList())
+
+  res.render('alert', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'alert',
+    info: 'Продукт було успішно cтворено',
+  })
+})
 router.get('/product-list', function (req, res) {
   res.render('product-list', {
     // вказуємо назву папки контейнера, в якій знаходяться наші стилі
@@ -102,17 +117,99 @@ router.get('/product-delete', function (req, res) {
   })
 })
 
-router.post('/product-update', function (req, res) {
-  const { data, id } = req.body
-  console.log(name, price, description, id)
+router.get('/product-edit', function (req, res) {
+  const { id } = req.query
+  console.log(name, price, id)
 
-  const result = Product.updateById(Number(id), { data })
+  const result = Product.updateById(Number(id), data)
+  res.render('success-info', {
+    style: 'success-info',
+    info: result
+      ? 'Товар оновлено'
+      : 'Товар з таким ID не знайдено',
+  })
+})
+
+router.post('/product-edit', function (req, res) {
+  const { id, name, price, description } = req.body
+  const result = Product.updateById(Number(id), {
+    name,
+    price,
+    description,
+  })
+
   res.render('success-info', {
     style: 'success-info',
     info: result ? 'Товар оновлено' : 'Сталась помилка',
   })
 })
 
+// router.get('/product-edit', async function (req, res) {
+//   const { id } = req.query
+
+//   if (!id) {
+//     return res.render('/alert', {
+//       style: 'danger',
+//       info: 'Необхідно вказати ID товару',
+//     })
+//   }
+
+//   try {
+//     const product = await Product.findById(Number(id))
+
+//     if (!product) {
+//       return res.render('/alert', {
+//         style: 'danger',
+//         info: 'Товар з таким ID не знайдено',
+//       })
+//     }
+
+//     res.render('/product-edit', { product })
+//   } catch (error) {
+//     console.error(error)
+//     res.render('/alert', {
+//       style: 'danger',
+//       info: 'Помилка отримання даних товару',
+//     })
+//   }
+// })
+
+// router.post('/product-edit', async function (req, res) {
+//   const { id, name, price, description } = req.body
+
+//   if (!id || !name || !price || !description) {
+//     return res.render('/alert', {
+//       style: 'danger',
+//       info: 'Необхідно заповнити всі поля',
+//     })
+//   }
+
+//   try {
+//     const result = await Product.updateById(Number(id), {
+//       name,
+//       price,
+//       description,
+//     })
+
+//     if (result) {
+//       res.render('/alert', {
+//         style: 'success',
+//         info: 'Товар успішно оновлено',
+//       })
+//     } else {
+//       res.render('/alert', {
+//         style: 'danger',
+//         info: 'Помилка оновлення товару',
+//       })
+//     }
+//   } catch (error) {
+//     console.error(error)
+//     res.render('/alert', {
+//       style: 'danger',
+//       info: 'Помилка оновлення даних товару',
+//     })
+//   }
+// })
 // ================================================================
 
 // Підключаємо роутер до бек-енду
