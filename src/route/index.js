6368e1865a4f7802a00997aedba22bfc1047912c
ElinaMrachkovska <elinamrachkovska@gmail.com>
2025-03-27 22:07:59 +0200

    scss

--- src/route/index.js
+++ src/route/index.js
@@ -189,38 +189,37 @@
 // // Підключаємо роутер до бек-енду
 // module.exports = router
 
-const express = require('express')
-const router = express.Router()
+// const express = require('express')
+// const router = express.Router()
 
 // ================================================================
+
+const express = require('express')
+const router = express.Router()
 
 class Product {
   static #list = []
 
   static #count = 0
 
-  constructor(img, title, description, category, price) {
+  constructor(
+    img,
+    title,
+    description,
+    category,
+    price,
+    amount = 0,
+  ) {
     this.id = ++Product.#count
     this.img = img
     this.title = title
     this.description = description
     this.category = category
     this.price = price
+    this.amount = amount
   }
-  static add = (
-    img,
-    title,
-    description,
-    category,
-    price,
-  ) => {
-    const newProduct = new Product(
-      img,
-      title,
-      description,
-      category,
-      price,
-    )
+  static add = (...data) => {
+    const newProduct = new Product(...data)
     this.#list.push(newProduct)
   }
   static getList = () => {
@@ -245,13 +244,14 @@ class Product {
 }
 Product.add(
   './img/comp/616.png',
-  "Комп'ютер Artline Gaming (X43v31) AMD Ryzen 5 3600",
-  'AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 16 ГБ / HDD 1 ТБ + SSD 480 ГБ / nVidia GeForce RTX 3050, 8 ГБ / без ОД / LAN / без ОС',
+  `Комп'ютер Artline Gaming (X43v31) AMD Ryzen 5 3600`,
+  `AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 16 ГБ / HDD 1 ТБ + SSD 480 ГБ / nVidia GeForce RTX 3050, 8 ГБ / без ОД / LAN / без ОС`,
   [
     { id: 2, text: 'Готовий до відправки' },
     { id: 1, text: 'Топ продажів' },
   ],
   27000,
+  10,
 )
 Product.add(
   './img/comp/617.png',
@@ -259,6 +259,7 @@ Product.add(
   'Intel Core i3-10100F (3.6 - 4.3 ГГц) / RAM 8 ГБ / HDD 1 ТБ + SSD 240 ГБ / GeForce GTX 1050 Ti, 4 ГБ / без ОД / LAN / Linux',
   [{ id: 1, text: 'Топ продажів' }],
   17000,
+  10,
 )
 
 Product.add(
@@ -267,6 +268,7 @@ Product.add(
   'Intel Core i9-13900KF (3.0 - 5.8 ГГц) / RAM 64 ГБ / SSD 2 ТБ (2 x 1 ТБ) / nVidia GeForce RTX 4070 Ti, 12 ГБ / без ОД / LAN / Wi-Fi / Bluetooth / без ОС',
   [{ id: 1, text: 'Топ продажів' }],
   113009,
+  10,
 )
 
 Product.add(
@@ -278,6 +280,7 @@ Product.add(
     { id: 1, text: 'Топ продажів' },
   ],
   27000,
+  10,
 )
 Product.add(
   './img/comp/617.png',
@@ -285,6 +288,7 @@ Product.add(
   'Intel Core i3-10100F (3.6 - 4.3 ГГц) / RAM 8 ГБ / HDD 1 ТБ + SSD 240 ГБ / GeForce GTX 1050 Ti, 4 ГБ / без ОД / LAN / Linux',
   [{ id: 1, text: 'Топ продажів' }],
   17000,
+  10,
 )
 
 Product.add(
@@ -293,7 +297,11 @@ Product.add(
   'Intel Core i9-13900KF (3.0 - 5.8 ГГц) / RAM 64 ГБ / SSD 2 ТБ (2 x 1 ТБ) / nVidia GeForce RTX 4070 Ti, 12 ГБ / без ОД / LAN / Wi-Fi / Bluetooth / без ОС',
   [{ id: 1, text: 'Топ продажів' }],
   113009,
+  10,
 )
+class Purchase {
+  static DLIVERY_PRICE = 150
+}
 // router.get('/', function (req, res) {
 //   const list = Product.getList()
 
@@ -340,31 +348,78 @@ router.post('/purchase-create', function (req, res) {
 
   console.log(id, amount)
 
-  res.render('purchase-product', {
-    style: 'purchase-product',
-    data: {
-      list: Product.getRandomList(id),
-      product: Product.getById(id),
-    },
-  })
-})
-
-router.post('/purchase-create', function (req, res) {
-  const id = Number(req.query.id)
-  const amount = Number(req.body.amount)
-
-  console.log(id, amount)
-
   if (amount < 1) {
     return res.render('alert', {
       style: 'alert',
       data: {
-        massage: 'Помилка',
+        message: 'Помилка',
         info: 'Некоректна кількість товару',
         link: `/purchase-product?id=${іd}`,
       },
     })
   }
+
+  const product = Product.getById(id)
+
+  if (product.amount < 1) {
+    return res.render('alert', {
+      style: 'alert',
+      data: {
+        message: 'Помилка',
+        info: 'Такої кількості товару немає в наявності',
+        link: `/purchase-product?id=${іd}`,
+      },
+    })
+  }
+
+  console.log(product, amount)
+
+  // res.render('purchase-product', {
+  //   style: 'purchase-product',
+  //   data: {
+  //     list: Product.getRandomList(id),
+  //     product: Product.getById(id),
+  //   },
+  // })
+
+  const productPrice = product.price * amount
+  const totalPrice = productPrice + Purchase.DLIVERY_PRICE
+
+  res.render('purchase-create'),
+    {
+      style: 'purchase-create',
+      data: {
+        id: product.id,
+        cart: [
+          {
+            text: `${product.title} (${amount} шт)`,
+            price: productPrice,
+          },
+          {
+            text: 'Доставка',
+            price: Purchase.DLIVERY_PRICE,
+          },
+        ],
+        totalPrice,
+        productPrice,
+        deliveryPrice: Purchase.DLIVERY_PRICE,
+      },
+    }
+})
+
+router.post('/purchase-submit', function (req, res) {
+  console.log(req.query)
+  console.log(req.body)
+
+  res.render('alert', {
+    style: 'alert',
+
+    data: {
+      message: 'Успішно',
+      info: 'Замовлення створено',
+      link: `/purchase-list`,
+    },
+  })
 })
 
 // router.get('/product-create', function (req, res) {
