8d20bff01219dc0d694aa3ce827086e06ca3a34a
ElinaMrachkovska <elinamrachkovska@gmail.com>
2025-04-09 17:54:31 +0300

    09.04

--- src/route/index.js
+++ src/route/index.js
@@ -301,6 +301,58 @@ Product.add(
 )
 class Purchase {
   static DELIVERY_PRICE = 150
+  static #count = 0
+  static #list = []
+
+  constructor(data, product) {
+    this.id = ++Purchase.#count
+
+    this.firstname = data.firstname
+    this.lastname = data.lastname
+    this.phone = data.phone
+
+    this.email = data.email
+    this.comment = data.comment || null
+
+    this.bonus = data.bonus || 0
+    this.promocode = data.promocode || null
+
+    this.totalPrice = data.totalPrice
+    this.productPrice = data.productPrice
+    this.deliveryPrice = data.deliveryPrice
+    this.amount = amount
+
+    this.product = product
+  }
+
+  static add = (...arg) => {
+    const newPurchase = new Purchase(...arg)
+    this.#list.push(newPurchase)
+    return newPurchase
+  }
+
+  static getList = () => {
+    return Purchase.#list.reverse()
+  }
+  static getById = (id) => {
+    return Purchase.#list.find((item) => item.id === id)
+  }
+
+  static updateById = (id, data) => {
+    const purchase = Purchase.getById(id)
+
+    if (purchase) {
+      if (data.firstname) {
+        purchase.firstname = data.firstname
+        if (data.lastname) purchase.lastname = data.lastname
+        if (data.phone) purchase.phone = data.phone
+        if (data.email) purchase.email = data.email
+      }
+      return true
+    } else {
+      return false
+    }
+  }
 }
 router.get('/', function (req, res) {
   res.render('purchase-index', {
@@ -363,8 +415,6 @@ router.post('/purchase-create', function (req, res) {
     })
   }
 
-  console.log(product, amount)
-
   // res.render('purchase-product', {
   //   style: 'purchase-product',
   //   data: {
@@ -373,32 +423,81 @@ router.post('/purchase-create', function (req, res) {
   //   },
   // })
 
+  // console.log(product, amount)
+
   const productPrice = product.price * amount
   const totalPrice = productPrice + Purchase.DELIVERY_PRICE
 
-  res.render('purchase-create'),
-    {
-      style: 'purchase-create',
-      data: {
-        id: product.id,
-        cart: [
-          {
-            text: `${product.title} (${amount} шт)`,
-            price: productPrice,
-          },
-          {
-            text: 'Доставка',
-            price: Purchase.DELIVERY_PRICE,
-          },
-        ],
-        totalPrice,
-        productPrice,
-        deliveryPrice: Purchase.DELIVERY_PRICE,
-      },
-    }
+  res.render('purchase-create', {
+    style: 'purchase-create',
+    data: {
+      id: product.id,
+      cart: [
+        {
+          text: `${product.title} (${amount} шт)`,
+          price: productPrice,
+        },
+        {
+          text: 'Доставка',
+          price: Purchase.DELIVERY_PRICE,
+        },
+      ],
+      totalPrice,
+      productPrice,
+      deliveryPrice: Purchase.DELIVERY_PRICE,
+      amount,
+    },
+  })
 })
-
 router.post('/purchase-submit', function (req, res) {
+  const id = Number(req.query.id)
+
+  let {
+    totalPrice,
+    productPrice,
+    deliveryPrice,
+    amount,
+
+    firstname,
+    lastname,
+    phone,
+    email,
+  } = req.body
+
+  const product = Product.getById(id)
+
+  if (!product) {
+    return res.render('alert', {
+      style: 'alert',
+      data: {
+        message: 'Помилка',
+        info: 'Товар не знайдено',
+        link: `/purchase-list`,
+      },
+    })
+  }
+
+  totalPrice = Number(totalPrice)
+  productPrice = Number(productPrice)
+  deliveryPrice = Number(deliveryPrice)
+  amount = Number(amount)
+
+  if (
+    isNaN(totalPrice) ||
+    isNaN(productPrice) ||
+    isNaN(deliveryPrice) ||
+    isNaN(amount)
+  ) {
+    return res.render('alert', {
+      style: 'alert',
+      data: {
+        message: 'Помилка',
+        info: 'Некоректні дані',
+        link: `/purchase-list`,
+      },
+    })
+  }
+
   console.log(req.query)
   console.log(req.body)
 
